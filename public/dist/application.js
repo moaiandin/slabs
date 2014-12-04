8e233adc9558524b73d15ab6001b8423f9e023fc
Elliot Agro <elliotagro83@gmail.com>
2014-12-04 16:24:33 +0000

    improved plumbing

--- public/dist/application.js
+++ public/dist/application.js
@@ -475,6 +475,7 @@ angular.module('stage').config(['$stateProvider',
 // todo - tests for this class.
 // todo - use controllerAs syntax with a vm var.
 // todo - move bindable members to the top : https://github.com/johnpapa/angularjs-styleguide#style-y033
+// todo move jsPlumb to service or directive?
 
 angular.module('stage').controller('StageController', ['$scope','$state','SlabsServices','$sce',
 
@@ -528,6 +529,96 @@ angular.module('stage').controller('StageController', ['$scope','$state','SlabsS
 
 		};
 
+		var init = function(connection) {
+			connection.getOverlay("label").setLabel(connection.sourceId.substring(15) + "-" + connection.targetId.substring(15));
+			connection.bind("editCompleted", function(o) {
+				if (typeof console != "undefined")
+					console.log("connection edited. path is now ", o.path);
+			});
+		};
+
+		var initPlumb = function(){
+
+			window.instance = jsPlumb.getInstance({
+				// default drag options
+				DragOptions : { cursor: 'pointer', zIndex:2000 },
+				// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+				// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+				ConnectionOverlays : [
+					[ "Arrow", { location:1 } ],
+					[ "Label", {
+						location:0.1,
+						id:"label",
+						cssClass:"aLabel"
+					}]
+				],
+				Container:"stage-container"
+			});
+
+			var connectorPaintStyle = {
+				lineWidth:4,
+				strokeStyle:"#61B7CF",
+				joinstyle:"round",
+				outlineColor:"white",
+				outlineWidth:2
+			};
+			var connectorHoverStyle = {
+				lineWidth:4,
+				strokeStyle:"#216477",
+				outlineWidth:2,
+				outlineColor:"white"
+			};
+			var endpointHoverStyle = {
+				fillStyle:"#216477",
+				strokeStyle:"#216477"
+			};
+
+			var sourceEndpoint = {
+				endpoint:"Dot",
+				paintStyle:{
+					strokeStyle:"#7AB02C",
+					fillStyle:"transparent",
+					radius:4,
+					lineWidth:3
+				},
+				isSource:true,
+				connector:[ "Flowchart", { stub:[40, 60], gap:10, cornerRadius:5, alwaysRespectStubs:true } ],
+				connectorStyle:connectorPaintStyle,
+				hoverPaintStyle:endpointHoverStyle,
+				connectorHoverStyle:connectorHoverStyle,
+				dragOptions:{}
+			};
+
+			// the definition of target endpoints (will appear when the user drags a connection)
+			var targetEndpoint = {
+					endpoint:"Dot",
+					paintStyle:{ fillStyle:"#7AB02C",radius:7 },
+					hoverPaintStyle:endpointHoverStyle,
+					maxConnections:-1,
+					dropOptions:{ hoverClass:"hover", activeClass:"active" },
+					isTarget:true
+				};
+
+			window._addEndpoints = function(toId, sourceAnchors, targetAnchors) {
+				for (var i = 0; i < sourceAnchors.length; i++) {
+					var sourceUUID = toId + sourceAnchors[i];
+					instance.addEndpoint(toId, sourceEndpoint, { anchor:sourceAnchors[i], uuid:sourceUUID });
+				}
+				for (var j = 0; j < targetAnchors.length; j++) {
+					var targetUUID = toId + targetAnchors[j];
+					instance.addEndpoint(toId, targetEndpoint, { anchor:targetAnchors[j], uuid:targetUUID });
+				}
+			};
+
+		}
+
+		setTimeout(function(){
+			initPlumb();
+		}, 0);
+
+
+
+
 		$('.stage').droppable({
 
 			drop: function( event, ui ) {
@@ -554,38 +645,31 @@ angular.module('stage').controller('StageController', ['$scope','$state','SlabsS
 					top		:top
 				};
 
+				console.log(slab);
+
 				// add slab to slab network
 				$scope.slabs.push(slab);
 				$scope.$digest();
 
+				_addEndpoints(slabID, ["BottomLeft", "BottomCenter", "BottomRight"], ["TopLeft", "TopCenter", "TopRight"]);
 
-				jsPlumb.makeSource($('.source'), {
-					anchor: 'Continuous'
-				});
-				jsPlumb.makeTarget($('.target'), {
-					anchor: 'Continuous'
-				});
-
-				jsPlumb.draggable($('.panel'), {
-					containment:'#stageContainer'
+				// listen for new connections; initialise them the same way we initialise the connections at startup.
+				instance.bind("connection", function(connInfo, originalEvent) {
+					init(connInfo.connection);
 				});
 
+				instance.draggable(jsPlumb.getSelector(".stage-container .panel"), { grid: [20, 20] });
 
 			}
 
 		});
 
+		// add sumbit data function
 		window.submitSlabData = function(data){
 			$scope.settingsPageVisible = false;
 			$scope.$digest();
 		};
 
-
-		jsPlumb.ready(function() {
-
-			jsPlumb.setContainer($('#stageContainer'));
-
-		});
 
 	}
 
